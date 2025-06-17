db858c05fc438106b73efda53169e35187672251
Flaubert SEO <flaubertweb@gmail.com>
2025-06-17 14:16:59 -0300

    Primeiro commit subindo arquivos iniciais

---
+++ app.py
@@ -0,0 +1,46 @@
+
+# Imagine que você foi contratado para desenvolver um sistema inovador de
+# gerenciamento de reservas para um hotel boutique em uma cidade turística.
+
+# O hotel, conhecido como "Refúgio dos Sonhos"
+
+# , precisa de um sistema que
+# permita gerenciar a disponibilidade dos quartos, as reservas dos hóspedes e
+# os dados dos clientes de forma eficiente e intuitiva.
+
+# Sua missão é criar uma aplicação interativa que atenda a essas necessidades
+# utilizando Programação Orientada a Objetos (POO) e a biblioteca Flet para
+# a interface gráfica.
+
+#___________________________________________________________________________________________
+
+# Utilize o Flet para construir uma interface gráfica que contenha as seguintes estruturas:
+
+# Tela de visualização de reservas:
+# Apresente uma lista de reservas existentes com a
+# opção de cancelar reservas específicas.
+
+# Tela inicial:
+# Exiba uma lista de quartos e suas respectivas
+# disponibilidades.
+# Inclua botões para realizar reservas, consultar
+# informações de clientes e gerenciar reservas.
+
+# Formulário de reserva:
+# Permita que o usuário escolha o cliente, o quarto e
+# as datas desejadas para criar uma nova reserva.
+
+# Gerenciamento de clientes:
+# Inclua uma tela para visualizar, adicionar e editar
+# informações dos clientes.
+
+import flet as ft
+from model import *
+
+def main(page:ft.Page):
+    pass
+
+
+ft.app(target=main)
+
+
---
+++ model.py
@@ -0,0 +1,37 @@
+
+class Cliente:
+    def __init__(self, nome, telefone, email, id):
+        self.nome
+        self.telefone
+        self.email
+        self.id
+
+class Quarto:
+    def __init__(self, numero_do_quarto, tipo, preco, status):
+        self.numero_do_quarto
+        self.tipo
+        self.preco
+        self.status        
+
+class Reserva:
+    def __init__(self, dono_da_reserva, quarto_reservado, data_check_in, data_check_out):
+        self.dono_da_reserva
+        self.quarto_reservado
+        self.data_check_in
+        self.data_check_out                
+
+class GerenciadorDeReservas:
+    def Verificar_disponibilidade(self):
+        pass
+
+    def criar_reserva(self):
+        pass
+
+    def moficiar_reserva(self):
+        pass
+
+    def cancelar_reserva(self):
+        pass
+
+    def listar_reservas(self):
+        pass
